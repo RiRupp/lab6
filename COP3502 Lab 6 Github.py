a558b7515a48de1f529ea9720f3c937bee0718cd
RiRupp <rruppjobs@gmail.com>
2024-10-20 19:21:19 -0600

    added .py file, created main(), displayMenu(), encode() and their functions. Also added decode(), but did not add any function.

---
+++ COP3502 Lab 6 Github.py
@@ -0,0 +1,59 @@
+# Ryan Rupp - Created original empty repo, main(), encoder() functions
+# Brody Rayhill - Will create decoder() function
+
+# The purpose of this program is take a string of integers as a password and add the ability to encode and decode it.
+
+def main():
+    while True:
+        displayMenu()
+        user_input = input("Please enter an option: ")
+        if int(user_input) == 1:
+            encoded_password = encode()
+        elif int(user_input) == 2:
+            decode()
+        elif int(user_input) == 3:
+            exit()
+        else:
+            print("Invalid option. Please try again.")
+            print()
+
+def displayMenu():
+    print("Menu\n-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print()
+    return
+
+def encode():
+    pw_check = False
+    while pw_check == False: # Checks for valid password input
+        password = input("Please enter your password to encode: ")
+        if len(password) == 8: # Make sure password is 8 chars long
+            count = 0
+            for i in password:
+                if i.isdigit(): # Make sure each digit of password is an integer
+                    count += 1
+                    if count == 8: pw_check = True
+                else:
+                    print("Invalid password. Please try again.")
+                    count = 0
+                    break
+        else:
+            print("Invalid password. Please try again.")
+    password_list = []
+    for i in password:
+        num = int(i)
+        password_list.append(num)
+    for i in range(len(password_list)):
+        password_list[i] = str(password_list[i] + 3)
+    encoded_password = ''.join(password_list)
+    print("Your password has been encoded and stored!")
+    print()
+    return encoded_password
+
+def decode():
+    pass
+
+if __name__ == "__main__":
+    main()
